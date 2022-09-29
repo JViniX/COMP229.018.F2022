7b31810cb07dfbd0280bfbe6e624bd791d8d02b7
JViniX <47263194+JViniX@users.noreply.github.com>
2022-09-28 20:10:12 -0400

    EJS explained

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render(
+    'index', 
+    { 
+      title: 'Home', 
+      name: 'Julio'
+    }
+  );
 });
 
-
 router.get('/julio', function(req, res, next) {
-  res.render('index', { title: 'Julio' });
+  res.render('main',  { 
+    title: 'Julio', 
+    name: 'Julio'
+  });
 });
 
 
