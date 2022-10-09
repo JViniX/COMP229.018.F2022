b7736aa54ae85c25f6823c54d1c2d830a8ea1a2f
JViniX <47263194+JViniX@users.noreply.github.com>
2022-10-08 23:27:24 -0400

    CRUD Express

---
+++ controllers/index.js
@@ -0,0 +1,16 @@
+exports.home = function(req, res, next) {
+    res.render(
+      'index', 
+      { 
+        title: 'Home', 
+        name: 'Julio'
+      }
+    );
+  }
+
+exports.projects = function(req, res, next) {
+    res.render('index',  { 
+      title: 'Projects', 
+      name: 'Julio'
+    });
+  }
--- routes/users.js
+++ routes/users.js
@@ -3,17 +3,23 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/julio', function(req, res, next) {
-  res.render('users',  { 
-    title: 'Users', 
-    name: 'Julio'
-  });
+  res.render(
+    'users', 
+    { 
+      title: 'Julio', 
+      name: 'Julio' 
+    }
+  );
 });
 
 router.get('/john', function(req, res, next) {
-  res.render('users',  { 
-    title: 'Users', 
-    name: 'John'
-  });
+  res.render(
+    'users', 
+    { 
+      title: 'John Smith', 
+      name: 'John Smith' 
+    }
+  );
 });
 
 module.exports = router;
